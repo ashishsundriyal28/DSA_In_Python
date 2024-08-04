b2bd9ae0e3b4322710e613caf99fd6596cfd7727
Ashish Sundriyal <ashishsundriyal028@gmail.com>
2024-08-04 20:33:41 +0530

    Minor change

--- 04_Array_List_CODING_EXERCISES_LEETCODE/Ques_01_Missing_Number.py
+++ 04_Array_List_CODING_EXERCISES_LEETCODE/Ques_01_Missing_Number.py
@@ -1,6 +1,6 @@
-# --------------------------------------
+# -------------------------------------
 # Ques 1. Missing Number
-# --------------------------------------
+# -------------------------------------
 # Write a function to find the missing number 
 # in a given integer array of 1 to 100. 
 # The function takes to parameter the array 

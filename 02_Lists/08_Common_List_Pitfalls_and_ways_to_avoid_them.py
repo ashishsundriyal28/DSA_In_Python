13e39ea57dcf7b6aaedbf7cb84a2491c5acbcbb7
Ashish Sundriyal <98693345+ashishsundriyal28@users.noreply.github.com>
2024-09-02 15:51:28 +0530

    Delete 02_Lists directory

--- 02_Lists/08_Common_List_Pitfalls_and_ways_to_avoid_them.py
+++
@@ -1,9 +0,0 @@
-myList = [8,9,41,2,3,1]
-# orig = myList[:]
-# myList.sort()
-# print(orig)     # output = [8, 9, 41, 2, 3, 1]
-# print(myList)   # output = [1, 2, 3, 8, 9, 41]
-
-
-print(sorted(myList))   # output = [1, 2, 3, 8, 9, 41]
-print(myList)   # output = [8, 9, 41, 2, 3, 1]

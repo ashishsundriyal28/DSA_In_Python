2a5ff3c033713ab97cb59d68c86c0b2808852d54
Ashish Sundriyal <ashishsundriyal@cdac.in>
2024-12-26 16:19:29 +0530

    sec-4 Q4

--- 004_Array_List_CODING_EXERCISES_LEETCODE/Ques_04_Max_Product_of_Two_Integers.py
+++ 004_Array_List_CODING_EXERCISES_LEETCODE/Ques_04_Max_Product_of_Two_Integers.py
@@ -1,3 +1,12 @@
+# --------------------------------------------------------------------------
+# Ques 3. Max Product of Two Integers
+# Find the maximum product of two integers in an array where all elements are positive.  
+
+# Example
+# arr = [1, 7, 3, 4, 9, 5]
+# max_product(arr) # Output: 63 (9*7)
+# --------------------------------------------------------------------------
+
 def max_product(arr):
     max1,max2=0,0
     for num in arr:
